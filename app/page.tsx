5233427547ca25c23dc5b9a2a84df54cec52ad7e
ranjam <ryanzam2005@gmail.com>
2025-07-08 18:34:30 +0545

    👩‍💻 add login and dashboard components

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,26 @@
+'use client'
+
 import { Tally5 } from "lucide-react";
 import Loading from "./components/ui/Loading";
+import { useSession } from "next-auth/react";
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
 
 export default function Home() {
+
+  const { data: session, status } = useSession()
+  const router = useRouter()
+
+  useEffect(() => {
+    if (status === 'loading') return
+
+    if (session) {
+      router.push('/dashboard')
+    } else {
+      router.push('/auth/signin')
+    }
+  }, [])
+
   return (
     <div className="min-h-screen bg-gradient-to-br from-blue-50 to-indigo-100 flex items-center justify-center">
       <div className="text-center">
