ef49925210948e882e6a49f4c945a5d7eafde76a
ranjam <ryanzam2005@gmail.com>
2025-07-08 18:08:08 +0545

    🏚 add home page and  install icons

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,17 @@
-import Image from "next/image";
+import { Tally5 } from "lucide-react";
+import Loading from "./components/ui/Loading";
 
 export default function Home() {
   return (
-    <>Home</>
+    <div className="min-h-screen bg-gradient-to-br from-blue-50 to-indigo-100 flex items-center justify-center">
+      <div className="text-center">
+        <div className="flex items-center justify-center">
+          <Tally5 className="h-16 w-16 text-blue-600" />
+        </div>
+        <h1 className="font-bold mb-3">SkyhubERP</h1>
+        <Loading />
+        <p className="mt-4 text-gray-600">Loading your workspace...</p>
+      </div>
+    </div>
   );
 }
